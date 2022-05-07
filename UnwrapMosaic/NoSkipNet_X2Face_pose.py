56968ecb1705a54ed1d55d1db55583b00fe10075
asa-asa <63086574+asa-asa@users.noreply.github.com>
2022-05-08 00:51:22 +0800

    Update NoSkipNet_X2Face_pose.py

--- UnwrapMosaic/NoSkipNet_X2Face_pose.py
+++ UnwrapMosaic/NoSkipNet_X2Face_pose.py
@@ -17,10 +17,7 @@ def weights_init_normal(m):
     classname = m.__class__.__name__
     # print(classname)
     if classname.find('Conv') != -1:
-        init.uniform(m.weight.data, 0.0, 
-                    
-                    
-                    )
+        init.uniform(m.weight.data, 0.0, 0.02)
     elif classname.find('Linear') != -1:
         init.uniform(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm2d') != -1:
